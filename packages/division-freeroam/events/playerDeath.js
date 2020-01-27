add8ffef6a48873860abce80c05b22f2c73f6051
MrPancakers <br3ttscott@gmail.com>
2020-01-27 18:25:47 +1000

    Implemented Hospital spawns and money loss on death

--- packages/division-freeroam/events/playerDeath.js
+++ packages/division-freeroam/events/playerDeath.js
@@ -1,5 +1,8 @@
 mp.events.add("playerDeath", (player, reason, killer) => {
+    let money = player.getMoney() - 50;
     let deaths = player.getVariable('deaths');
+    player.setVariable('isDead', true);
+    player.setMoney(money);
     player.setVariable('deaths', deaths + 1);
     player.deathTimer = setTimeout(() => {
         server.auth.spawnPlayer(player);
